3c10aadd297b780e2447f5705ee5dd444063f035
carkat <nathan.rogers.d@gmail.com>
2018-08-05 14:03:03 -0600

    oops

--- entity.lisp
+++ entity.lisp
@@ -10,10 +10,10 @@
     (make-instance 'entity :name name :hp hp :atk atk))
 
 (defgeneric attack (entity target))
-(defmethod attack ((e entity) target)
+(defmethod attack ((e entity) (target entity))
     (let ((thp (slot-value target 'hp))
         (atk (slot-value e 'atk))))
-        (setf (slot-value target'hp) (- thp atk))))
+        (setf (slot-value target 'hp) (- thp atk))))
 
 (defvar enemy  (make-entity "bad guy" 10 10))
 (defvar player (make-entity "player"  10 10))
