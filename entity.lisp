b1cc3064fef01973175b5c518c4584f2a6395d75
carkat <nathan.rogers.d@gmail.com>
2018-08-06 01:05:03 -0600

    classes

--- entity.lisp
+++ entity.lisp
@@ -1,3 +1,8 @@
+(defun prompt-read (prompt)
+  (format *query-io* "~a: "prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
 (defclass entity ()
     ((name
       :initarg :name
@@ -8,19 +13,30 @@
      (atk
       :initarg :atk
       :accessor atk)))
+
+(defclass player (entity) ())
     
 (defun make-entity (name hp atk)
     (make-instance 'entity :name name :hp hp :atk atk))
 
-(defgeneric attack (entity target))
-(defgeneric take-damag (entity target))
-(defmethod attack ((e entity) (target entity))
-    (let ((thp (hp target))
-        (atk (atk e)))
-        (setf (hp target) (- thp atk))))
+(defun make-player (hp atk)
+    (make-instance 'player :name (prompt-read "What is your name") :hp hp :atk atk))
 
-(defvar enemy  (make-entity "bad guy" 10 20))
-(defvar player (make-entity "player"  10 10))
+(defgeneric take-damage (entity amt))
+(defmethod take-damage ((e entity) amt)
+    (setf (hp e) (- (hp e) amt)))
+
+(defgeneric attack (entity target))
+(defmethod attack ((e entity) (target entity))
+    (take-damage target (atk e)))
+
+
+(defmethod attack ((p player) (target entity))
+    (print "What do you want to do? (take action based on player input)")
+    (take-damage target (atk p)))
+
+(defvar enemy  (make-entity "bad guy" 20 20))
+(defvar player (make-player 10 10))
 
 (attack player enemy)
 (print (hp enemy))
